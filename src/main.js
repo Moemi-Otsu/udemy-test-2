d5fe17a37c5e9c73bfba53436642121b39414afe
moemi ohtsu <s0623192@gmail.com>
2021-01-29 23:56:40 +0900

    vuerouter入れた

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Swiper from 'vue-awesome-swiper/dist/vue-awesome-swiper.esm'
+import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter.js'
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+// Vue.use(VueAwesomeSwiper);
+Vue.use(getAwesomeSwiper(Swiper));
 
 new Vue({
+  router: router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+		<swiper class="swiper" :options="swiperOption">
+				<swiper-slide>Slide 1</swiper-slide>
+				<swiper-slide>Slide 2</swiper-slide>
+				<swiper-slide>Slide 3</swiper-slide>
+				<swiper-slide>Slide 4</swiper-slide>
+				<swiper-slide>Slide 5</swiper-slide>
+				<swiper-slide>Slide 6</swiper-slide>
+				<swiper-slide>Slide 7</swiper-slide>
+				<swiper-slide>Slide 8</swiper-slide>
+				<swiper-slide>Slide 9</swiper-slide>
+				<swiper-slide>Slide 10</swiper-slide>
+				<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
+
+export default {
+  name: 'swiper-example-pagination',
+  title: 'Pagination',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        autoplay: {
+          delay: 5000
+        }
+      }
+    }
+  }
+}
+</script>
